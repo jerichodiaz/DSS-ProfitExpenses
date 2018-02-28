359d3d06bb07d043b0b050de8e0f4e9295976911
Keeboi <kiboy5@yahoo.com>
2018-02-28 14:24:25 +0800

    Created ui

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,72 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 /**
  * Created by Diaz, Jericho Hans
  * On 2/28/2018
  */
 public class Controller {
+    @FXML private ProgressIndicator piResource;
+    @FXML
+    private RadioButton rResource, rHR, rOverhead, rMaterials, rMaterialCost,
+            rTotalCost, rEmployeeCount, rSalary, rOperationExpense, rQuota, rRent,
+            rAdvertising, rMaintenance, rExpense, rProducts, rPrice, rRevenue;
+    @FXML
+    private TextField txtResource, txtHR, txtOverhead, txtMaterials, txtMaterialCost,
+            txtTotalCost, txtEmployeeCount, txtSalary, txtOperationExpense, txtQuota, txtRent,
+            txtAdvertising, txtMaintenance, txtExpense, txtProducts, txtPrice, txtRevenue;
+    @FXML
+    private PieChart chart, chartHR, chartOverhead;
+    private ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
+    private ObservableList<PieChart.Data> hrData = FXCollections.observableArrayList();
+    private ObservableList<PieChart.Data> overheadData = FXCollections.observableArrayList();
+
+    private void inputChartData() {
+
+        chart.setLabelsVisible(false);
+        chart.setLegendVisible(true);
+        chart.setLegendSide(Side.RIGHT);
+        chartHR.setLabelLineLength(5);
+        chartOverhead.setLabelLineLength(5);
+        chartHR.setLegendVisible(false);
+        chartOverhead.setLegendVisible(false);
+
+
+
+        chart.setData(data);
+        data.clear();
+        data.addAll(
+                new PieChart.Data("Resource", Double.parseDouble(txtResource.getText())),
+                new PieChart.Data("HR Management", Double.parseDouble(txtHR.getText())),
+                new PieChart.Data("Overhead Bills", Double.parseDouble(txtOverhead.getText()))
+        );
+        chartHR.setData(hrData);
+        hrData.clear();
+        hrData.addAll(
+                new PieChart.Data("Salary", Double.parseDouble(txtSalary.getText())),
+                new PieChart.Data("Operation", Double.parseDouble(txtOperationExpense.getText()))
+        );
+        chartOverhead.setData(overheadData);
+        overheadData.clear();
+        overheadData.addAll(
+                new PieChart.Data("Rent", Double.parseDouble(txtRent.getText())),
+                new PieChart.Data("Advertising", Double.parseDouble(txtAdvertising.getText())),
+                new PieChart.Data("Maintenance", Double.parseDouble(txtMaintenance.getText()))
+        );
+    }
+
+    protected void calculate() {
+        inputChartData();
+        double x = Double.parseDouble(txtResource.getText());
+        double y = Double.parseDouble(txtHR.getText());
+        double z = Double.parseDouble(txtOverhead.getText());
+        double a = x+y+z;
+        piResource.setProgress(x/a);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /**
@@ -8,14 +13,31 @@ import javafx.stage.Stage;
  * On 2/28/2018
  */
 public class Main extends Application{
+    Controller controller;
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("ui.fxml"));
 
+        controller = new Controller();
+        loader.setControllerFactory( c -> controller);
+        Scene scene = new Scene(loader.load());
         primaryStage.setTitle("Profit Expenses");
-        primaryStage.setScene(new Scene(loader.load()));
+        primaryStage.setScene(scene);
         primaryStage.show();
+        addShortcuts(scene);
+    }
 
+    private void addShortcuts(Scene scene){
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+            final KeyCombination keyCombRun = new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_DOWN);
+            @Override
+            public void handle(KeyEvent ke) {
+                if(keyCombRun.match(ke)){
+                    controller.calculate();
+                    ke.consume();
+                }
+            }
+        });
     }
 
     public static void main(String[] args) {
